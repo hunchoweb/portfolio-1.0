19ab1dc9d3c409643f0b9626f7558ce457269d33
Ojowa Odunayo <hunchoweb@gmail.com>
2023-09-12 11:30:01 +0100

    updated project component

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["links.papareact.com", "cdn.sanity.io", "i.imgur.com"],
+        domains: ["links.papareact.com", "cdn.sanity.io", "i.imgur.com", "www.webdesignerdepot.com"],
     },}
 
 module.exports = nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import About from '@/components/About';
 import Header from '@/components/Header'
 import Hero from '@/components/Hero';
+import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
 import WorkExperience from '@/components/WorkExperience';
 // import Image from 'next/image'
@@ -23,12 +24,14 @@ export default function Home() {
         <WorkExperience />
       </section>
 
-      {/* Skills */}
       <section id='skills' className='snap-start'>
         <Skills />
       </section>
 
       {/* Projects  */}
+      <section id='projects' className='snap-start'>
+        <Projects />
+      </section>
 
       {/* Contact Me */}
 
